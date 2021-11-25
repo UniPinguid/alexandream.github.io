bf82bfa7f4ae93091af1dcbc3543a7e95e66ec64
UniPinguid <bao.phat.50@gmail.com>
2021-11-25 10:04:19 +0700

    Upload sample

---
+++ Sandbox/MySlider.js
@@ -0,0 +1,9 @@
+﻿// MySlider
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
---
+++ Sandbox/myCheckbox.js
@@ -0,0 +1,12 @@
+﻿var attackSpeed = 1;
+
+function buffSS() {
+    if (document.getElementById("check").click) {
+        attackSpeed *= 1.176;
+    }
+    else {
+        attackSpeed /= 1.176;
+    }
+}
+
+document.getElementById("atk-result").innerHTML = attackSpeed;
