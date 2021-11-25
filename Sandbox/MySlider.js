810f5c91b9302780d7b4a0be673ee1dd45e10ea1
UniPinguid <bao.phat.50@gmail.com>
2021-11-25 10:06:44 +0700

    Remove old samples

--- Sandbox/MySlider.js
+++
@@ -1,9 +0,0 @@
-﻿// MySlider
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-    output.innerHTML = this.value;
-}
